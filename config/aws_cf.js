3783e89c7f0a3cd6778569798cb9b66f23299174
Md Mushahid Ansari <md.mushahidansari@gmail.com>
2026-02-06 19:37:18 +0530

    "increase the expire time of cloudfront signed url "

--- config/aws_cf.js
+++ config/aws_cf.js
@@ -3,7 +3,7 @@ import { getSignedUrl } from "@aws-sdk/cloudfront-signer";
 const cloudfrontDistributionDomain = "https://d24gtp8x4ffd63.cloudfront.net";
 const privateKey = process.env.CLOUDFRONT_KEY.replace(/\\n/g, "\n");
 const keyPairId = "K1WWF25J0YVIQ7";
-const dateLessThan = new Date(Date.now() + 5 * 60 * 1000).toISOString()
+const dateLessThan = new Date(Date.now() + 15 * 60 * 1000).toISOString()
 
 const cloudfrontSignedUrl = (s3ObjectKey, filename, download = false) => {
   const url = `${cloudfrontDistributionDomain}/${s3ObjectKey}?download=${download}&filename=${encodeURIComponent(filename)}`;
